b63f7f961f2a469c61442f7ccaba3886e4e89259
小hangzi_ <isgonghang@163.com>
2016-11-20 20:30:23 +0800

    Add files via upload

---
+++ js/showPic.js
@@ -0,0 +1,80 @@
+function addLoadEvent(func){
+	var oldonload=window.onload;
+	if (typeof window.onload!="function"){
+		window.onload=func;
+	} else {
+		window.onload=function(){
+			oldonload();
+			func();
+		}
+	}
+}
+function insertAfter(newElement,targetElemet) {
+	var parent=targetElemet.parentNode;
+	if (parent.lastChild==targetElemet){
+		parent.appendChild(newElement);
+	} else {
+		parent.insertBefore(newElement,targetElemet.nextSibling);
+	}
+}
+function preparePlaceholder(){
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false; 
+	var placeholder=document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/placeholder.gif");
+    placeholder.setAttribute("alt","my image gallery");
+    var description=document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext=document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery=document.getElementById("imagegallery");
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+}
+function prepareGallery(){
+	if (!document.getElementsByTagName）{
+		return false;
+	}
+    if (!document.getElementById) {
+    	return false;
+    }
+    if (!document.getElementById("imagegallery")) {
+    	return false;
+    }
+    var gallery=document.getElementById("imagegallery");
+    var links=gallery.getElementsByTagName("a");
+    for (var i = 0;i<links.length;i++){
+    	links[i].onclick=function(){
+          return !showPic(this);
+    }
+    links[i].onkeypress=links[i].onclick;
+  } 
+}
+
+
+
+
+function showPic(whichpic) {
+	if (!document.getElementById("placeholder")) return true;
+	var source=whichpic.getAttribute("href");
+	var placeholder=document.getElementById("placeholder")
+	placeholder.setAttribute("src",source);
+	if (document.getElementById("description")) return false;
+	if (whichpic.getAttribute("title")){
+		var text=whichpic.getAttribute("title");
+	} else {
+		var text="";
+	}
+	var description=document.getElementById("description");
+	if (description.firstChild.nodeValue==3){
+		description.firstChild.nodeValue=text;
+	}
+	return false;
+}
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+	
+
